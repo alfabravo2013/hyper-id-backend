022d775413490d9695ba3f1d5a8d62f7a7d5ad9e
Viktor Martynov <2013alfabravo@gmail.com>
2022-11-08 15:35:08 +0400

    Restore OpenAPI

--- src/main/java/com/github/alfabravo2013/hyperidbackend/api/HyperUserController.java
+++ src/main/java/com/github/alfabravo2013/hyperidbackend/api/HyperUserController.java
@@ -1,5 +1,12 @@
 package com.github.alfabravo2013.hyperidbackend.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -25,19 +32,41 @@ public class HyperUserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create user", description = "Register a new user with username and password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "409", description = "username already taken")})
     @PostMapping(path = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> register(@RequestBody HyperUserCredentials credentials) {
+    public ResponseEntity<Void> register(
+            @Parameter(
+                    description = "username and password container",
+                    content = @Content(schema = @Schema(implementation = HyperUserCredentials.class)))
+            @RequestBody HyperUserCredentials credentials) {
         LOGGER.debug("Registering: {}", credentials);
 
         userService.register(credentials);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Authenticate user", description = "Submit user credentials to get an access token")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    headers = {
+                            @Header(name = "accessToken", description = "Access token"),
+                            @Header(name = "sessionId", description = "Session ID") },
+                    content = @Content(schema = @Schema(implementation = HyperUserDto.class))
+            ),
+            @ApiResponse(responseCode = "401", description = "Bad credentials") })
     @PostMapping(
             path = "/login",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> login(@RequestBody HyperUserCredentials credentials) {
+    public ResponseEntity<?> login(
+            @Parameter(
+                    description = "username and password container",
+                    schema = @Schema(implementation = HyperUserCredentials.class))
+            @RequestBody HyperUserCredentials credentials) {
         LOGGER.debug("Logging in: {}", credentials);
 
         var map = userService.login(credentials);
@@ -52,32 +81,62 @@ public class HyperUserController {
         return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
+    @Operation(summary = "Log out", description = "invalidates provided access token")
+    @ApiResponse(responseCode = "200", description = "successful operation")
     @PostMapping(path = "/logout")
     public ResponseEntity<?> logout(
+            @Parameter(name = "Authorization", description = "access token")
             @RequestHeader("Authorization") String token) {
         userService.logout(token);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Get user details", description = "retrieves user profile associated with provided access token")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "successful operation",
+                    content = @Content(schema = @Schema(implementation = HyperUserDto.class))),
+            @ApiResponse(responseCode = "404", description = "no account associated with provided token")
+    })
     @GetMapping(path = "/account", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HyperUserDto> getAccount(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<HyperUserDto> getAccount(
+            @Parameter(name = "Authorization", description = "access token")
+            @RequestHeader("Authorization") String token) {
         LOGGER.debug("Found Authorization: {}", token);
+
         var body = userService.getAccount(token);
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update user details", description = "changes user name, surname and password")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "successful operation",
+                    content = @Content(schema = @Schema(implementation = HyperUserDto.class))),
+            @ApiResponse(responseCode = "404", description = "no account associated with provided token")
+    })
     @PutMapping(
             path = "/account",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> updateAccount(
+            @Parameter(name = "Authorization", description = "authorization token")
             @RequestHeader("Authorization") String token,
+            @Parameter(
+                    name = "new details",
+                    description = "user details container",
+                    content = @Content(schema = @Schema(implementation = HyperUserUpdateDto.class)))
             @RequestBody HyperUserUpdateDto userDto) {
         LOGGER.debug("Setting new user details: {}", userDto);
+
         var body = userService.updateAccount(token, userDto);
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete all", description = "clears all accounts from the database")
+    @ApiResponse(responseCode = "200", description = "successful operation")
     @DeleteMapping(path = "/account")
     public ResponseEntity<Void> deleteAll() {
         userService.deleteAllUsers();
---
+++ src/main/java/com/github/alfabravo2013/hyperidbackend/config/ApiDocConfig.java
@@ -0,0 +1,25 @@
+package com.github.alfabravo2013.hyperidbackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ApiDocConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("HyperID ")
+                        .version("0.1")
+                        .description("Hyperskill Full Stack Project")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
